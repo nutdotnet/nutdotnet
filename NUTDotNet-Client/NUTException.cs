380ea71a1db5c2f4d0b5dbc6d99e29e4224f4ca0
tgp1994 <tgp1994@users.noreply.github.com>
2021-01-25 18:04:28 -0800

    Moved response error info to exception class - Moved much of the error information from the Response class to a new exception class. - Response is now a real constructor, and throws the new expcetion is a problem occurs. - Testing is still needed on all of this.

---
+++ NUTDotNet-Client/NUTException.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUTDotNetClient
+{
+    class NUTException : Exception
+    {
+        public enum Error
+        {
+            ACCESSDENIED,
+            UNKNOWNUPS,
+            VARNOTSUPPORTED,
+            CMDNOTSUPPORTED,
+            INVALIDARGUMENT,
+            INSTCMDFAILED,
+            SETFAILED,
+            READONLY,
+            TOOLONG,
+            FEATURENOTSUPPORTED,
+            FEATURENOTCONFIGURED,
+            ALREADYSSLMODE,
+            DRIVERNOTCONNECTED,
+            DATASTALE,
+            ALREADYLOGGEDIN,
+            INVALIDPASSWORD,
+            ALREADYSETPASSWORD,
+            INVALIDUSERNAME,
+            ALREADYSETUSERNAME,
+            USERNAMEREQUIRED,
+            PASSWORDREQUIRED,
+            UNKNOWNCOMMAND,
+            INVALIDVALUE
+        }
+
+        public static Error ErrorCode;
+        // The full error response from the server.
+        public static string RawError;
+        /// <summary>
+        /// Extra data provided by the error message. Currently unimplemented in the NUT protocol.
+        /// </summary>
+        public string ErrorExtraData
+        {
+            get
+            {
+                throw new NotImplementedException("This protocol feature is not yet implemented.");
+            }
+        }
+
+        /// <summary>
+        /// Try to parse an error string, beginning with ERR, into the associated Error enumeration.
+        /// </summary>
+        /// <param name="rawError"></param>
+        /// <returns></returns>
+        private Error ParseErrorCode(string rawError)
+        {
+            // The rest of the string after ERR will be hyphens. Strip them out and parse the message into an enum.
+            string errMessage = rawError.Substring(4).Replace("-", String.Empty);
+            return (Error)Enum.Parse(typeof(Error), errMessage);
+        }
+
+        protected NUTException() : base("An undefined exception has occurred.") { }
+
+        public NUTException(string rawError) : base(rawError)
+        {
+            RawError = rawError;
+            ErrorCode = ParseErrorCode(rawError);
+        }
+
+        public NUTException(string rawError, string message) : base(message)
+        {
+            RawError = rawError;
+            ErrorCode = ParseErrorCode(rawError);
+        }
+
+        public NUTException(string rawError, string message, Exception innerException)
+            : base(message, innerException)
+        {
+            RawError = rawError;
+            ErrorCode = ParseErrorCode(rawError);
+        }
+    }
+}
--- NUTDotNet-Client/Response.cs
+++ NUTDotNet-Client/Response.cs
@@ -4,33 +4,6 @@ namespace NUTDotNetClient
 {
     class Response
     {
-        public enum Error
-        {
-            ACCESSDENIED,
-            UNKNOWNUPS,
-            VARNOTSUPPORTED,
-            CMDNOTSUPPORTED,
-            INVALIDARGUMENT,
-            INSTCMDFAILED,
-            SETFAILED,
-            READONLY,
-            TOOLONG,
-            FEATURENOTSUPPORTED,
-            FEATURENOTCONFIGURED,
-            ALREADYSSLMODE,
-            DRIVERNOTCONNECTED,
-            DATASTALE,
-            ALREADYLOGGEDIN,
-            INVALIDPASSWORD,
-            ALREADYSETPASSWORD,
-            INVALIDUSERNAME,
-            ALREADYSETUSERNAME,
-            USERNAMEREQUIRED,
-            PASSWORDREQUIRED,
-            UNKNOWNCOMMAND,
-            INVALIDVALUE
-        }
-
         /// <summary>
         /// The time when the associated request was initiated.
         /// </summary>
@@ -49,26 +22,10 @@ namespace NUTDotNetClient
                 return TimeReceived - TimeInitiated;
             }
         }
-
-        /// <summary>
-        /// If the response received was successful ("OK" or data is returned) this is true. False if ERR is returned.
-        /// </summary>
-        public bool Success { get; private set; }
-        public Error ErrorCode { get; private set; }
         /// <summary>
         /// Data provided from the response, if applicable. Note this may be null.
         /// </summary>
         public Object Data { get; private set; }
-        /// <summary>
-        /// Extra data provided by the error message. Currently unimplemented in the NUT protocol.
-        /// </summary>
-        public string ErrorExtraData
-        {
-            get
-            {
-                throw new NotImplementedException("This protocol feature is not yet implemented.");
-            }
-        }
 
         /// <summary>
         /// Parse the response from a NUT server, determine if it was successful or not, separate the information, and
@@ -78,7 +35,7 @@ namespace NUTDotNetClient
         /// <param name="timeReceived"></param>
         /// <param name="responseData"></param>
         /// <returns></returns>
-        public Response ParseResponse(DateTime timeInitiated, DateTime timeReceived, string responseData)
+        public Response(DateTime timeInitiated, DateTime timeReceived, string responseData)
         {
             TimeInitiated = timeInitiated;
             TimeReceived = timeReceived;
@@ -88,8 +45,6 @@ namespace NUTDotNetClient
 
             if (responseData.StartsWith("OK"))
             {
-                Success = true;
-
                 // If there's more to the string than just the response, get that as well.
                 if (responseData.Length > 2)
                 {
@@ -98,20 +53,13 @@ namespace NUTDotNetClient
             }
             else if(responseData.StartsWith("ERR"))
             {
-                Success = false;
-
-                // The rest of the string will be hyphens. Strip them out and parse the message into an enum.
-                string errMessage = responseData.Substring(4).Replace("-", String.Empty);
-                ErrorCode = (Error)Enum.Parse(typeof(Error), errMessage);
+                throw new NUTException(responseData);
             }
             // Likely some complex data was returned. Dump it as-is and send it back for processing.
             else
             {
-                Success = true;
                 Data = responseData;
             }
-
-            return this;
         }
     }
 }
