70ce9c02f82f6203a7364f1240333d16f623ab26
tgp1994 <tgp1994@users.noreply.github.com>
2021-01-27 17:37:59 -0800

    Simplified NUTException class - Moved Error enum and error code parsing back to Response class - Corrected access modifiers - Commenting out unused constructors for now (may need later) - Will be unit tested as a part of the Response class

--- NUTDotNet-Client/NUTException.cs
+++ NUTDotNet-Client/NUTException.cs
@@ -1,41 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace NUTDotNetClient
 {
-    class NUTException : Exception
+    /// <summary>
+    /// A thrown exception when a valid error code is returned from the NUT server.
+    /// </summary>
+    public class NUTException : Exception
     {
-        public enum Error
-        {
-            ACCESSDENIED,
-            UNKNOWNUPS,
-            VARNOTSUPPORTED,
-            CMDNOTSUPPORTED,
-            INVALIDARGUMENT,
-            INSTCMDFAILED,
-            SETFAILED,
-            READONLY,
-            TOOLONG,
-            FEATURENOTSUPPORTED,
-            FEATURENOTCONFIGURED,
-            ALREADYSSLMODE,
-            DRIVERNOTCONNECTED,
-            DATASTALE,
-            ALREADYLOGGEDIN,
-            INVALIDPASSWORD,
-            ALREADYSETPASSWORD,
-            INVALIDUSERNAME,
-            ALREADYSETUSERNAME,
-            USERNAMEREQUIRED,
-            PASSWORDREQUIRED,
-            UNKNOWNCOMMAND,
-            INVALIDVALUE
-        }
-
-        public static Error ErrorCode;
+        public readonly Response.Error ErrorCode;
         // The full error response from the server.
-        public static string RawError;
+        public readonly string RawError;
         /// <summary>
         /// Extra data provided by the error message. Currently unimplemented in the NUT protocol.
         /// </summary>
@@ -47,37 +21,24 @@ namespace NUTDotNetClient
             }
         }
 
-        /// <summary>
-        /// Try to parse an error string, beginning with ERR, into the associated Error enumeration.
-        /// </summary>
-        /// <param name="rawError"></param>
-        /// <returns></returns>
-        private Error ParseErrorCode(string rawError)
-        {
-            // The rest of the string after ERR will be hyphens. Strip them out and parse the message into an enum.
-            string errMessage = rawError.Substring(4).Replace("-", String.Empty);
-            return (Error)Enum.Parse(typeof(Error), errMessage);
-        }
+        //protected NUTException() : base("An undefined exception has occurred.") { }
 
-        protected NUTException() : base("An undefined exception has occurred.") { }
-
-        public NUTException(string rawError) : base(rawError)
+        /*public NUTException(string rawError) : base(rawError)
         {
             RawError = rawError;
-            ErrorCode = ParseErrorCode(rawError);
-        }
+        }*/
 
-        public NUTException(string rawError, string message) : base(message)
+        public NUTException(string rawError, Response.Error errorCode) : base()
         {
             RawError = rawError;
-            ErrorCode = ParseErrorCode(rawError);
+            ErrorCode = errorCode;
         }
 
-        public NUTException(string rawError, string message, Exception innerException)
-            : base(message, innerException)
+        /*public NUTException(string rawError, Response.Error errorCode, Exception innerException)
+            : base(String.Empty, innerException)
         {
             RawError = rawError;
-            ErrorCode = ParseErrorCode(rawError);
-        }
+            ErrorCode = errorCode;
+        }*/
     }
 }
