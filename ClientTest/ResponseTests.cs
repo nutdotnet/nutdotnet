a7fe232a6e8e5cf549b2e9e4ffdaa84b0fd029a9
tgp1994 <tgp1994@users.noreply.github.com>
2021-01-29 14:09:28 -0800

    Finished unit testing response class, all tests pass

--- ClientTest/ResponseTests.cs
+++ ClientTest/ResponseTests.cs
@@ -13,7 +13,37 @@ namespace ClientTest
         [Fact]
         public void TryParsingEmptyResponse()
         {
-            Assert.Throws<Exception>(() => new Response("", DateTime.Now, DateTime.Now));
+            Assert.Throws<ArgumentException>(() => new Response("", DateTime.Now, DateTime.Now));
+        }
+
+        /// <summary>
+        /// Verify that different time parameters behave as expected.
+        /// </summary>
+        [Fact]
+        public void VerifyTimesBehaviors()
+        {
+            Response testResponse;
+            DateTime sampleTimeStamp = DateTime.Now;
+
+            // Start with null times. Should be default values.
+            testResponse = new Response("OK", null, null);
+            Assert.Equal(default, testResponse.TimeInitiated);
+            Assert.Equal(default, testResponse.TimeReceived);
+            // RTT should give an exception here.
+            Assert.Throws<Exception>(() => testResponse.RoundTripTime);
+
+            // Try alternating null and valid values for time parameters. Both cases should throw an arg. exception.
+            Assert.Throws<ArgumentException>(() => new Response("OK", sampleTimeStamp, null));
+            Assert.Throws<ArgumentException>(() => new Response("OK", null, sampleTimeStamp));
+
+            // Try supplying a more recent time for the initiated argument. Should result in arg. exception.
+            Assert.Throws<ArgumentException>(() => new Response("OK", sampleTimeStamp.Add(new TimeSpan(1)), sampleTimeStamp));
+
+            // Now try a legitimate set of arguments, and verify the difference.
+            testResponse = new Response("OK", sampleTimeStamp, sampleTimeStamp.AddSeconds(1));
+            Assert.Equal(sampleTimeStamp, testResponse.TimeInitiated);
+            Assert.Equal(sampleTimeStamp.AddSeconds(1), testResponse.TimeReceived);
+            Assert.Equal(sampleTimeStamp.AddSeconds(1) - sampleTimeStamp, testResponse.RoundTripTime);
         }
 
         /// <summary>
@@ -24,17 +54,6 @@ namespace ClientTest
         {
             Response okResponse = new Response("OK", DateTime.Now, DateTime.Now);
             Assert.Null(okResponse.Data);
-        }
-
-        /// <summary>
-        /// Verify that null times are handled correctly (default to minimums)
-        /// </summary>
-        [Fact]
-        public void TryNullTimes()
-        {
-            Response nullTimesResponse = new Response("OK", null, null);
-            Assert.Equal(nullTimesResponse.TimeInitiated, DateTime.MinValue);
-            Assert.Equal(nullTimesResponse.TimeReceived, DateTime.MinValue);
         }
 
         /// <summary>
@@ -50,16 +69,35 @@ namespace ClientTest
         }
 
         /// <summary>
-        /// Try parsing error responses and see what the results are.
+        /// Try sending different valid raw error strings.
         /// </summary>
+        /// <param name="rawError"></param>
+        /// <param name="expectedEnum"></param>
         [Theory]
-        [InlineData("ERR ACCESS-DENIED", true)]
-        [InlineData("ERR UNKNOWN-UPS", true)]
-        [InlineData("ERR INVALID-USERNAME", true)]
-        [InlineData("ERR NOT-A-REAL-MESSAGE", false)]
-        public void TryErrorResponse(string response, bool shouldPass)
+        [InlineData("ERR ACCESS-DENIED", Response.Error.ACCESSDENIED)]
+        [InlineData("ERR UNKNOWN-UPS", Response.Error.UNKNOWNUPS)]
+        [InlineData("ERR USERNAME-REQUIRED", Response.Error.USERNAMEREQUIRED)]
+        public void TryValidErrors(string rawError, Response.Error expectedEnum)
         {
+            try
+            {
+                new Response(rawError);
+            }
+            catch (NUTException ex)
+            {
+                Assert.Equal(rawError, ex.RawError);
+                Assert.Equal(expectedEnum, ex.ErrorCode);
+            }
+        }
 
+        /// <summary>
+        /// Try sending an invalid raw error string.
+        /// </summary>
+        /// <param name="rawError"></param>
+        [Fact]
+        public void TryParsingInvalidError()
+        {
+            Assert.Throws<ArgumentException>(() => new Response("ERR NOT-A-REAL-ERROR"));
         }
     }
 }
