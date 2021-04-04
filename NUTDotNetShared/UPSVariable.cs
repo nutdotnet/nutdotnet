b851398bd0606ae30476b45ae17cc6a39bd5832a
gbakeman <gbakeman@live.com>
2021-04-04 16:21:53 -0700

    Modifying UPS variable structure to be more like NUT implementation. - Modfying UPS variable implementation to be more like the NUT server & protocol, which uses a generic structure to represent different kinds of data. - This became necessary while implementing the GET TYPE and GET DESC queries. - Ref. issue #21.

---
+++ NUTDotNetShared/UPSVariable.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NUTDotNetShared
+{
+    /// <summary>
+    /// Specifies properties of a UPSVariable (state). Multiple flags can be given. String and number are mutually
+    /// exclusive (not String implies Number).
+    /// </summary>
+    [Flags]
+    public enum Flags
+    {
+        None = 0,
+        RW = 1,
+        String = 2,
+        Number = 4,
+        Immutable = 8
+    }
+
+    /// <summary>
+    /// Implements a state, or variable from a UPS. A state's value is limited to 256 characters (although the
+    /// UPS C code says this could be made dynamic). A state can have multiple Flags, Enumerations, Ranges,
+    /// and Commands.
+    /// Refer to https://github.com/networkupstools/nut/blob/master/include/extstate.h
+    /// </summary>
+    public class UPSVariable
+    {
+        private string varValue;
+        private Flags flags;
+
+        public static readonly int MAX_VALUE_LENGTH = 256;
+        public string Name;
+        public string Value
+        {
+            get
+            {
+                return varValue;
+            }
+            set
+            {
+                if (value.Length > MAX_VALUE_LENGTH)
+                    throw new ArgumentOutOfRangeException("Value", "Value cannot be longer than MAX_VALUE_LENGTH.");
+                else
+                    varValue = value;
+            }
+        }
+        public Flags Flags
+        {
+            get
+            {
+                return flags;
+            }
+            set
+            {
+                if (value == (Flags.String & Flags.Number))
+                    throw new ArgumentException("A variable cannot be both a string and a number.");
+                else
+                    flags = value;
+            }
+        }
+        public List<string> Enumerations;
+        // The second dimension of the array is a length of 2, representing a min and max of the range.
+        public int[,] Ranges;
+        public List<string> InstantCommands;
+    }
+}
