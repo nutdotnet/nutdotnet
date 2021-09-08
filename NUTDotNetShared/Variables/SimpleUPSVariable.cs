7d44d5d24d454c61016bcc3e7d6f1cab6402a60b
gbakeman <gbakeman@live.com>
2021-09-07 17:37:42 -0700

    Implementing new structure for UPS variables. - Ref. issue #23

---
+++ NUTDotNetShared/Variables/SimpleUPSVariable.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NUTDotNetShared.Variables
+{
+    /// <summary>
+    /// Specifies properties of a UPSVariable (state). Multiple flags can be given. String and number are mutually
+    /// exclusive (not String implies Number).
+    /// </summary>
+    [Flags]
+    public enum VarFlags
+    {
+        None = 0,
+        RW = 1,
+        String = 2,
+        Number = 4,
+        Immutable = 8 //Seems to only block server from changing flags later, not much use to a client.
+    }
+
+    /// <summary>
+    /// Implements a state, or variable from a UPS. A state's value is limited to 256 characters (although the
+    /// UPS C code says this could be made dynamic). A state can have multiple Flags, Enumerations, Ranges,
+    /// and Commands.
+    /// Refer to https://github.com/networkupstools/nut/blob/master/include/extstate.h
+    /// </summary>
+    class SimpleUPSVariable<T> : IEquatable<SimpleUPSVariable<T>> where T : IConvertible // https://stackoverflow.com/a/8746643/530172
+    {
+        public static readonly int MAX_VALUE_LENGTH = 256;
+
+        private VarFlags flags;
+        // An int or string
+        private T varValue;
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public T Value {
+            get
+            {
+                return varValue;
+            }
+
+            set
+            {
+                if (value.ToString().Length > MAX_VALUE_LENGTH)
+                    throw new ArgumentOutOfRangeException("Value",
+                        "Value exceeds the maximum allowed length: " + MAX_VALUE_LENGTH);
+
+                varValue = value;
+            }
+        }
+
+        public VarFlags Flags
+        {
+            get
+            {
+                return flags;
+            }
+            set
+            {
+                if (value.HasFlag(VarFlags.String | VarFlags.Number))
+                    throw new ArgumentException("A variable cannot be both a string and a number.");
+                else
+                    flags = value;
+            }
+        }
+
+        public SimpleUPSVariable(string name, string description = NUTCommon.NULL_TEXT, VarFlags flags = VarFlags.None)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Name variable cannot be blank or null.");
+
+            Name = name;
+            Description = description;
+            Flags = flags;
+        }
+
+        #region Base methods
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as SimpleUPSVariable<T>);
+        }
+
+        /// <summary>
+        /// Two variables may still be considered identical if at least one of the descriptions is null or empty.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Equals(SimpleUPSVariable<T> obj)
+        {
+            return Name == obj.Name &&
+                (Description == obj.Description || String.IsNullOrEmpty(Description) || String.IsNullOrEmpty(obj.Description)) &&
+                IComparer<T>.Equals(Value, obj.Value) && //Little trick courtesy of https://stackoverflow.com/a/390974/530172
+                Flags == obj.Flags;
+        }
+
+        /// <summary>
+        /// Override hash code. Variable names must be unique, so we're only hashing that.
+        /// Quote from .NET Docs: "You should not assume that equal hash codes imply object equality."
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 53;
+                hash *= 23 + Name.GetHashCode();
+                return hash;
+            }
+        }
+
+        #endregion
+    }
+}
