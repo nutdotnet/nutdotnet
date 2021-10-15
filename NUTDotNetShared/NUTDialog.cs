eee5e486c384f0e3bc93b8cfa9ddc63c856c0361
gbakeman <gbakeman@live.com>
2021-10-15 16:13:23 -0700

    Shared NUT protocol communication classes - Created a basis for representing communication in the NUT protocol with class objects. Currently implements a Query with parsing, response to come soon. - Added a text list of NUT protocol commands for reference.

---
+++ NUTDotNetShared/NUTDialog.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NUTDotNetShared
+{
+    /// <summary>
+    /// Represents a single conversation between client and server; a <see cref="NUTQuery"/> followed by a
+    /// <see cref="Response"/>
+    /// </summary>
+    public abstract class NUTDialog
+    {
+        // public enum NUTSubject { NUMLOGINS, UPSDESC, VAR, TYPE, DESC, CMDDESC, UPS, RW, CMD, ENUM, RANGE, CLIENT }
+
+        #region Private Members
+
+        #endregion
+
+        #region Properties
+
+        public NUTQuery Query { get; private set; }        
+        /// <summary>
+        /// The original Query of the dialog, broken down into components separated by spaces.
+        /// </summary>
+        // public string[] Query { get; private set; }
+
+        
+        //public NUTSubject Subject { get; private set; }
+        //public string UPSName { get; private set; }
+        //public string VarName { get; private set; }
+        //public string Value { get; private set; }
+        // public AbstractUPS UPS { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initiates a NUT dialog, beginning with the query.
+        /// </summary>
+        /// <param name="query"></param>
+        public NUTDialog(string query)
+        {
+            Query = new NUTQuery(query);
+        }
+
+        public NUTDialog(NUTQuery query)
+        {
+            Query = query;
+        }
+
+        #endregion
+
+        
+
+        
+    }
+}
---
+++ NUTDotNetShared/NUTQuery.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NUTDotNetShared
+{
+    public class NUTQuery
+    {
+        public enum NUTCommand { GET, LIST, SET, INSTCMD, LOGOUT, LOGIN, PASSWORD, USERNAME, MASTER, FSD, STARTTLS, HELP, VER, NETVER }
+
+        // Match a set of words (seperated by spaces), handling quoted phrases as one unit.
+        public static Regex QUERY_MATCH = new Regex(" (?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))", RegexOptions.Compiled);
+
+        public NUTCommand Command { get; private set; }
+        public string[] Arguments { get; private set; }
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NUTQuery"/> class and parses a NUT protocol query.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        public NUTQuery(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+                throw new ArgumentNullException(nameof(query));
+
+            // Parse the query into its components, including quotation-wrapped phrases.
+            string[] splitQuery = QUERY_MATCH.Split(query);
+            // Count the number of extra words (arguments) proceeding the command.
+            int argumentCount = splitQuery.Length - 1;
+
+            // Parse the Command section. Throw a nicer exception if something goes wrong.
+            try
+            {
+                Command = (NUTCommand)Enum.Parse(typeof(NUTCommand), splitQuery[0]);
+            }
+            catch
+            {
+                throw new ArgumentException(splitQuery[0] + " is not a valid NUTCommand.");
+            }
+
+            // Move the arguments into the array property.
+            if (argumentCount > 0)
+            {
+                Arguments = new string[argumentCount];
+                Array.Copy(splitQuery, 1, Arguments, 0, argumentCount);
+            }
+        }
+
+        public NUTQuery(NUTCommand command, string[] arguments = null)
+        {
+            Command = command;
+
+            if (arguments is null)
+                Arguments = Array.Empty<string>();
+            else
+                Arguments = arguments;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder(Command.ToString());
+
+            foreach (string arg in Arguments)
+            {
+                sb.AppendFormat(" {0}", arg);
+            }
+
+            return sb.ToString();
+            // return base.ToString();
+        }
+
+        #endregion
+
+        #region Private methods
+
+        /// <summary>
+        /// Parses a valid NUT query and fills the object's NUTCommand and Arguments properties.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <exception cref="System.ArgumentNullException"></exception>
+        /// <exception cref="System.ArgumentException">The NUT command is invalid.</exception>
+        private void ParseQuery(string query)
+        {
+            
+
+            //if (Query.Length == 2)
+            //{
+            //    if (Command == NUTCommand.USERNAME || Command == NUTCommand.PASSWORD)
+            //        Value = Query[1];
+            //    else if (Command == NUTCommand.FSD || Command == NUTCommand.MASTER || Command == NUTCommand.LOGIN)
+            //        UPSName = Query[1];
+            //    else
+            //        throw new Exception("Unrecognized second argument: " + Query[1]);
+            //}
+
+            //else if (Query.Length > 2)
+            //{
+            //    // Handle final non-subject related second arg
+            //    if (Command == NUTCommand.INSTCMD)
+            //    {
+            //        UPSName = Query[1];
+            //        Value = Query[2];
+            //    }
+
+            //    // Only remaining possbilities are a subject
+            //    try
+            //    {
+            //        Subject = (NUTSubject)Enum.Parse(typeof(NUTSubject), Query[1]);
+
+            //        // Every valid subject besides LIST UPS, 3rd arg is upsname
+            //        if (Subject != NUTSubject.UPS)
+            //            UPSName = Query[2];
+
+            //        // Every valid subject with four (or five) arguments, 4th is a var(cmd)name.
+            //        if (Query.Length > 4)
+            //            VarName = Query[3];
+
+            //    }
+            //    catch
+            //    {
+            //        throw new ArgumentException(
+            //            Query[1] + " is not a valid subject for the " + Query[0] + " command.");
+            //    }
+            //}
+
+            //switch (Command)
+            //{
+            //    case NUTCommand.GET:
+
+            //}
+        }
+
+        #endregion
+    }
+}
